jenswi-linaro/hafnium

commit da72173c3721f3a259095fdb3c5d040f32b7b204

feat(mm): handle 52-bit PA (FEAT_LPA/2) as 48-bit.
FEAT_LPA introduces 52-bit PA width for 16KB and 64 KB granules.
Hafnium only supports 4KB granules, whose support for 52-bit PA is
introduced by FEAT_LPA2.

There are existing CPUs implementing FEAT_LPA, but the systems
currently don't make use of the 52 bits of address space.
Currently Hafnium recognizes 52-bit PA configuration, but boot is soon
stopped by failing 4KB support check.
Downgrading the feature to previous maximum address size of 48 bit
is reasonable, providing all the other software components
have the same limitation.

This patch downgrade PA size for both FEAT_LPA and FEAT_LPA2;
eventually FEAT_LPA2 will be correctly supported and this workaround
will apply when only FEAT_LPA is supported.

Change-Id: I9f80ba2e23baa19337b09a23f522cdc796c4f71d
Signed-off-by: Federico Recanati <federico.recanati@arm.com>

src/arch/aarch64/mm.c
@@ -741,7 +741,7 @@ uint64_t arch_mm_combine_table_entry_attrs(uint64_t table_attrs,
  */
 bool arch_mm_init(paddr_t table)
 {
-	static const int pa_bits_table[16] = {32, 36, 40, 42, 44, 48};
+	static const int pa_bits_table[16] = {32, 36, 40, 42, 44, 48, 52};
 	uint64_t features = read_msr(id_aa64mmfr0_el1);
 	uint64_t pe_features = read_msr(id_aa64pfr0_el1);
 	unsigned int nsa_nsw;
@@ -750,7 +750,7 @@ bool arch_mm_init(paddr_t table)
 	int sl0;
 
 	/* Check that 4KB granules are supported. */
-	if ((features >> 28) & 0xf) {
+	if (((features >> 28) & 0xf) == 0xf) {
 		dlog_error("4KB granules are not supported\n");
 		return false;
 	}
@@ -761,6 +761,14 @@ bool arch_mm_init(paddr_t table)
 			"Unsupported value of id_aa64mmfr0_el1.PARange: %x\n",
 			features & 0xf);
 		return false;
+	}
+
+	/* Downgrade PA size from 52 to 48 bits (FEAT_LPA workaround). */
+	if (pa_bits == 52) {
+		dlog_verbose(
+			"52-bit PA size not supported,"
+			" falling back to 48-bit\n");
+		pa_bits = 48;
 	}
 
 	dlog_info("Supported bits in physical address: %d\n", pa_bits);

src/arch/aarch64/hftest/mm.c
@@ -32,12 +32,12 @@ static uintreg_t mm_reset_sctlr_el1;
  */
 bool arch_vm_mm_init(void)
 {
-	static const int pa_bits_table[16] = {32, 36, 40, 42, 44, 48};
+	static const int pa_bits_table[16] = {32, 36, 40, 42, 44, 48, 52};
 	uint64_t features = read_msr(id_aa64mmfr0_el1);
 	int pa_bits = pa_bits_table[features & 0xf];
 
 	/* Check that 4KB granules are supported. */
-	if ((features >> 28) & 0xf) {
+	if (((features >> 28) & 0xf) == 0xf) {
 		dlog_error("4KB granules are not supported\n");
 		return false;
 	}

commit 659c79d5eacf32e8f5fcb1a6403a4de0f79199da

feat(mm): fix FEAT_LPA workaround
In commit da72173c3721 ("feat(mm): handle 52-bit PA (FEAT_LPA/2) as
48-bit.") the PA size is downgraded to 48 bits if a 52-bit PA
configuration is found. However, this isn't enough, for complete
downgrade the tcr_el2.ps field must be updated accordingly. So fix this
by setting tcr_el2.ps to 0x5 when downgrading to 48-bit PA size.

Fixes: da72173c3721 ("feat(mm): handle 52-bit PA (FEAT_LPA/2) as 48-bit.")
Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

src/arch/aarch64/mm.c
@@ -773,6 +773,7 @@ bool arch_mm_init(paddr_t table)
 	uint32_t pa_bits = arch_mm_get_pa_range();
 	uint32_t extend_bits;
 	uint32_t sl0;
+	uint32_t ps = features & 0xf;
 
 	/* Check that 4KB granules are supported. */
 	if (((features >> 28) & 0xf) == 0xf) {
@@ -794,6 +795,7 @@ bool arch_mm_init(paddr_t table)
 			"52-bit PA size not supported,"
 			" falling back to 48-bit\n");
 		pa_bits = 48;
+		ps = 0x5; /* 0b101 */
 	}
 
 	dlog_info("Supported bits in physical address: %d\n", pa_bits);
@@ -864,7 +866,7 @@ bool arch_mm_init(paddr_t table)
 		.vtcr_el2 =
 			(1U << 31) |		   /* RES1. */
 			(nsa_nsw << 29) |	   /* NSA/NSW. */
-			((features & 0xf) << 16) | /* PS, matching features. */
+			(ps << 16) |		   /* PS, matching features. */
 			(0 << 14) |		   /* TG0: 4 KB granule. */
 			(3 << 12) |		   /* SH0: inner shareable. */
 			(1 << 10) |  /* ORGN0: normal, cacheable ... */
@@ -939,7 +941,7 @@ bool arch_mm_init(paddr_t table)
 	} else {
 		arch_mm_config.tcr_el2 =
 			(1 << 20) |		   /* TBI, top byte ignored. */
-			((features & 0xf) << 16) | /* PS. */
+			(ps << 16) |		   /* PS. */
 			(0 << 14) |		   /* TG0, granule size, 4KB. */
 			(3 << 12) |		   /* SH0, inner shareable. */
 			(1 << 10) | /* ORGN0, normal mem, WB RA WA Cacheable. */